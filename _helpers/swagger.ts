44c342d56c91f845ebeabc4d79d59ead02c07af4
S3agui <cjcuerda.learns@gmail.com>
2026-05-23 16:48:43 +0800

    fix: use process.cwd() to locate swagger.yaml

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -5,29 +5,7 @@ import path from 'path';
 
 const router = express.Router();
 
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Node.js Sign-up and Verification API',
-            version: '1.0.0',
-            description: 'Node.js + MySQL - API with email sign-up, verification, authentication and forgot password'
-        },
-        servers: [
-            {
-                url: 'https://backend-repo-node-mysql-auth-api.onrender.com',
-                description: 'Production server'
-            },
-            {
-                url: 'http://localhost:4000',
-                description: 'Local development server'
-            }
-        ]
-    },
-    apis: ['./accounts/*.ts']
-};
-
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
 
 router.use('/', swaggerUI.serve);
 router.get('/', swaggerUI.setup(swaggerDocument));
